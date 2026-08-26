f710be57ec84c9a294fcf40a42e1c69a0ee161d8
Jose Lucas <joselucasww2@hotmail.com>
2026-08-25 22:29:45 -0300

    feat: algoritmo do agachamento de 130kg concluido com sucesso

---
+++ desafioAgacho.js
@@ -0,0 +1,16 @@
+
+
+
+let pesoCorporal = 67;
+let carga = 130;
+
+let primeiraSerie = 6; 
+let segundaSerie = 5;
+let repeticoes = primeiraSerie + segundaSerie
+
+
+let volumeTotal = carga * repeticoes;
+
+
+console.log(`Hoje você destruiu e moveu ${volumeTotal}kg no agacho `);
+
