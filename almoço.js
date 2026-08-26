48026457266b6c66dd6f4e587b68fb651c644cef
Jose Lucas <joselucasww2@hotmail.com>
2026-08-26 15:43:18 -0300

    feat: evolucao do app nutricional com a sintaxe do loop for of

--- almoço.js
+++ almoço.js
@@ -1,6 +1,6 @@
 function calcularCalorias (alimento, gramas){
 let caloriaPorGrama = 0;
-   
+
     if (alimento==='carne'){
         caloriaPorGrama = 2.5
     }
@@ -10,10 +10,32 @@ let caloriaPorGrama = 0;
      else if ( alimento === 'batata'){
         caloriaPorGrama = 0.52
      }
-      
-
-
-     return (`${gramas} de ${alimento} da um total de ${caloriaPorGrama * gramas} calorias!`)
+     const formaDeCalcular = caloriaPorGrama * gramas;  
+     return formaDeCalcular;
+      //antigo estava assim: return (`${gramas} de ${alimento} da um total de ${caloriaPorGrama * gramas} calorias!`)
+    //return caloriaPorGrama * gramas
+ 
+ 
 
 }
-console.log(calcularCalorias('carne',100));
+
+let prato = [ { nome: 'carne', gramas: 100 }, { nome: 'arroz', gramas: 0 },{nome: 'batata', gramas: 0}];
+let totalCaloriasAlmoco =0;
+for(const alimento of prato ) 
+totalCaloriasAlmoco += calcularCalorias(alimento.nome, alimento.gramas);
+
+
+console.log(`O total do prato combinado deu: ${totalCaloriasAlmoco} calorias! 🥩🍚`);
+
+
+
+
+//totalCaloriasAlmoco += calcularCalorias('carne',150);
+
+
+
+
+
+
+//Daqui pra cima consegui fazer para calcular alimento por alimento, agora que scriar uma variável tipo 
+// "calorias totais" e aponte o total das calorias de alimentos unidos em um almoço
