01095eff3dd6ccd929d3ccdda6a4a9089ec54273
Jose Lucas <joselucasww2@hotmail.com>
2026-09-01 07:00:27 -0300

    Feat: Priemiro codigo estruturado em C com estruturas de decisao do tema 5

---
+++ teste.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+int cargaAgachamento = 130;
+  if(cargaAgachamento >= 130){
+    printf("Treino pesado! carga de elite ativada!\n");
+  }
+   else{
+    printf("Aquecimento ou regeneração do hardware.\n");
+}
+return 0;
+}
