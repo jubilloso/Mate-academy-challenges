afd8626a1ab04a8d5af9cbd0027f84443d1cc471
Jose Lucas <joselucasww2@hotmail.com>
2026-09-01 17:00:54 -0300

    Feat: primeiro codigo em C com declarações condicionais relacionadas a treino de força

--- teste.c
+++ teste.c
@@ -1,11 +1,63 @@
+/*#include <stdio.h>
+
+int main (void)
+{
+	float nota;
+	printf ("Digite a nota: ");
+	scanf("%f", &nota);
+	printf("%0f\n",nota);
+	if (nota >= 5)
+		printf("Parabéns você foi aprovado\n");
+	if (nota < 5)
+		printf("Não deu desta vez, mas não desista!");
+	return 0;
+}*/
+
 #include <stdio.h>
-int main(){
-int cargaAgachamento = 130;
-  if(cargaAgachamento >= 130){
-    printf("Treino pesado! carga de elite ativada!\n");
+int main (){
+int repeticoes;
+int carga;
+char postura;
+char opcao;
+float pesoCorporal;
+
+printf("\nBem vindo! Responda com S ou N \n");
+
+printf ("Voce quer testar Sensor Inteligente do Powerlifter? \n");
+scanf("%c", &opcao);
+    if (opcao == 'S' || opcao == 's'){
+       
+    printf("\nVamos ver se voce pode aumentar a carga:\n");
+    
+    printf("\nQual o seu peso corporal?");
+    scanf("%f", &pesoCorporal);
+
+    printf("Qual a sua carga: \n");
+    scanf("%i", &carga);
+    
+    printf("Quantas repeticões você consegue com essa carga?\n");
+    scanf("%i", &repeticoes);
+    
+    printf("A postura estava correta? S/N: \n");
+    
+    scanf(" %c", &postura);
   }
-   else{
-    printf("Aquecimento ou regeneração do hardware.\n");
+    
+  if (opcao == 'N' || opcao == 'n' ){ 
+      opcao = 0;
+    printf ("\nOk, não vamos continuar\n");
+  
+      return 0;
+  }
+if ((repeticoes > 7) && (postura == 's'|| postura == 'S')){
+printf("\n Parabéns monstro, voce pode subir essa carga! O céu é o limite");
+printf("Você está levantando %.2f vezes seu peso corporal!\n", (float) carga / pesoCorporal);
 }
-return 0;
+else{
+  printf("Tente dominar mais essa carga antes de progredir");
 }
+
+return 0; 
+}
+
+\\ ideias : criar um ranking de força: intermediário, avancado... e calcular um RM 
