694e7794300d5eeb6850d30a1d1dd0df622e48a4
Jose Lucas <joselucasww2@hotmail.com>
2026-09-23 09:37:26 -0300

    Feat: Primeiro desafio em C concluido Desenvolva um programa que leia um número e o mostre 20 vezes.

---
+++ Exemplos em c/desafioem.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+int number, cont;
+
+
+printf("\nDigite um numero: ");
+scanf("%d", &number);
+
+for (cont = 1; cont <= 5; cont ++){
+    
+    printf("\nnumero lido: %d ",number);
+}
+return 0;
+    
+}
